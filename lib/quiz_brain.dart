81b6b2b1ffe558711d2e3799bfc96f953a1f4ef2
Akash Jain <akash.jain.nagarro@gmail.com>
2020-05-12 13:55:00 +0530

    initalize

---
+++ lib/quiz_brain.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:quizzler/question.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
+
+class QuizBrain {
+  var _questionNumber = 0;
+  List<Icon> _scoreKeeper = [];
+  List<Question> _questions = [
+    Question('Some cats are actually allergic to humans', true),
+    Question('You can lead a cow down stairs but not up stairs.', false),
+    Question('Approximately one quarter of human bones are in the feet.', true),
+    Question('A slug\'s blood is green.', true),
+    Question('Buzz Aldrin\'s mother\'s maiden name was \"Moon\".', true),
+    Question('It is illegal to pee in the Ocean in Portugal.', true),
+    Question(
+        'No piece of square dry paper can be folded in half more than 7 times.',
+        false),
+    Question(
+        'In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.',
+        true),
+    Question(
+        'The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.',
+        false),
+    Question(
+        'The total surface area of two human lungs is approximately 70 square metres.',
+        true),
+    Question('Google was originally called \"Backrub\".', true),
+    Question(
+        'Chocolate affects a dog\'s heart and nervous system; a few ounces are enough to kill a small dog.',
+        true),
+    Question(
+        'In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.',
+        true),
+  ];
+
+  String getQuestion() => _questions[_questionNumber].question;
+  bool getAnswer() => _questions[_questionNumber].answer;
+
+  void gotoNextQuestion(BuildContext context) {
+    if (_questionNumber < _questions.length - 1)
+      _questionNumber++;
+    else {
+      Alert(
+              context: context,
+              title: "Finished",
+              desc: "You have reached end of test.")
+          .show();
+      _questionNumber = 0;
+      _scoreKeeper.clear();
+    }
+  }
+
+  get scoresKeeper => _scoreKeeper;
+
+  void checkAnswer(bool value) {
+    if (_questionNumber < _questions.length - 1)
+      _scoreKeeper.add(Icon(
+        getAnswer() == value ? Icons.check : Icons.close,
+        color: getAnswer() == value ? Colors.green : Colors.red,
+      ));
+  }
+}
